e366c5c65e6891aef5890ad5fba162cf67b99598
Yawar <yawarwani.263@gmail.com>
2025-01-29 23:32:59 +0530

    29-01-25

--- 5. Strings/09-duplicates-in-a-string.c
+++ 5. Strings/09-duplicates-in-a-string.c
@@ -9,13 +9,11 @@ int main(int argc, char const *argv[])
         if(A[i]>='a' && A[i]<='z') H[A[i]-'a']+=1;
         else if(A[i]>='A' && A[i]<='Z') H[A[i]-'A'+ 26]+=1;
     }
-    for (int i = 0; i < 26; i++)
+    for (int i = 0; i < 52; i++)
     {
-        if(H[i]>1) printf("%c appears %d times\n",i+'a',H[i]);
-    }
-    for (int i = 26; i < 52; i++)
-    {
-        if(H[i]>1) printf("%c appears %d times\n",i+'A'-26,H[i]);
+        if(i<26){
+             if(H[i]>1) printf("%c appears %d times\n",i+'a',H[i]);
+        } else if(H[i]>1)printf("%c appears %d times\n",i+'A'-26,H[i]);  
     }
     return 0;
 }
